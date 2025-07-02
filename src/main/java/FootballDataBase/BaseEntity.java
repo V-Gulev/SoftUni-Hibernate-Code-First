29ba1672fa26ab97718635cd9180b4e4679cba09
V-Gulev <velizargulev@abv.bg>
2025-07-02 22:12:57 +0300

    Football BaseEntity

---
+++ src/main/java/FootballDataBase/BaseEntity.java
@@ -0,0 +1,11 @@
+package FootballDataBase;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+}
